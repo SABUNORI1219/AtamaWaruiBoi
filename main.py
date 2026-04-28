c286b9a9a1f196af7830098823219c9bc894fe36
SABUNORI1219 <sabunori1219@gmail.com>
2026-04-28 09:33:25 +0900

    Initialize main.py with imports and environment variables

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+import discord
+from discord.ext import commands
+import genshin
+
+# 環境変数
+TOKEN = os.getenv("DISCORD_BOT_TOKEN")
+LTUID = os.getenv("HOYOLAB_LTUID")
+LTOKEN = os.getenv("HOYOLAB_LTOKEN")
+
+# Shori
